8f8630349fd44ddb952b9173360a116d177badf4
Varsha Rao <varshar186@gmail.com>
2017-01-30 16:41:08 +0530

    Added check case for rm

--- clp.py
+++ clp.py
@@ -1,5 +1,9 @@
 import subprocess
+import sys
 val=raw_input("Enter value\n")
 args = val.split()
+if args[0] == 'rm'and args[1] = '\':
+    print "Danger don\'t even try it"
+    sys.exit()
 print args
 process = subprocess.Popen(args,shell=False)
