8d9a6d2b52c19b6df8d69e0355cd2d4ee2a4ac7a
Varsha Rao <varshar186@gmail.com>
2017-01-30 16:46:27 +0530

    Updated

--- clp.py
+++ clp.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 val=raw_input("Enter value\n")
 args = val.split()
-if args[0] == 'rm'and args[1] = '\':
+if args[0] == 'rm'and args[1] == '\\':
     print "Danger don\'t even try it"
     sys.exit()
 print args
